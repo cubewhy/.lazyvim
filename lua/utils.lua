34722e531206fd2b4f1672fe6cd43e0ba784754f
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-12-30 21:40:04 +0800

    feat: update nixos detection logic

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,9 +1,13 @@
 local M = {}
 function M.IsNixOS()
   local f = io.open("/run/current-system/sw", "r")
-  if f then
+  local libFolder = io.open("/usr/lib", "r")
+  if f and not libFolder then
     f:close()
     return true
+  end
+  if libFolder then
+    libFolder:close()
   end
   return false
 end
