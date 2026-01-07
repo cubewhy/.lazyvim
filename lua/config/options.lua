794deae3c4bf43907580847392b66b5a0c2b7db1
cubewhy <61075476+cubewhy@users.noreply.github.com>
2026-01-07 18:09:59 +0800

    chore: change font size on Neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font:h13"
+  vim.o.guifont = "JetBrainsMono Nerd Font:h12"
   -- vim.o.guifont = "JetBrainsMono Nerd Font,Noto Color Emoji:h13"
   vim.g.neovide_floating_blur = false
   vim.g.neovide_floating_blur_amount_x = 0
@@ -15,7 +15,6 @@ if vim.g.neovide then
   -- vim.g.neovide_window_blurred = true
   -- vim.g.neovide_opacity = 0.8
   -- vim.g.neovide_normal_opacity = 0.8
-  -- vim.api.nvim_set_hl(0, "SnacksPicker", { bg = "NONE" })
 end
 
 vim.g.lazyvim_python_lsp = "basedpyright"
