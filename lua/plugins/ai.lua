0c59d469a75a978cb06aed2d7b000e302ebe7f2e
cubewhy <61075476+cubewhy@users.noreply.github.com>
2026-01-20 18:29:14 +0800

    feat: use qwen2.5-coder:7b as nvim.gen model

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -53,7 +53,7 @@ return {
     "David-Kunz/gen.nvim",
     config = function()
       require("gen").setup({
-        model = "mistral:7b", -- The default model to use.
+        model = "qwen2.5-coder:7b", -- The default model to use.
         quit_map = "q", -- set keymap to close the response window
         retry_map = "<c-r>", -- set keymap to re-send the current prompt
         accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
