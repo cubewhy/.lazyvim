d700635190548e39b2b42d3ea9cb1cf213bf87e9
cubewhy <qby140326@gmail.com>
2026-02-12 13:53:23 +0800

    refactor: rewrite the is_generic func

--- lua/plugins/autopair.lua
+++ lua/plugins/autopair.lua
@@ -4,60 +4,39 @@ return {
     event = { "InsertEnter", "CmdlineEnter" },
     branch = "v0.6",
     config = function()
-      local function is_lsp_generic_context()
-        local curr_win = vim.api.nvim_get_current_win()
-        local cursor = vim.api.nvim_win_get_cursor(curr_win)
-        local row, col = cursor[1] - 1, cursor[2]
-        local line = vim.api.nvim_get_current_line()
-        local char_after = line:sub(col + 1, col + 1)
-
-        if char_after == ">" then
-          return true
-        end
-
+      local function is_generic(o)
+        local line = o.line
+        local col = o.col
         local prefix = line:sub(1, col)
 
         if prefix:match("%s$") or prefix:match("%d$") or prefix == "" then
           return false
         end
 
-        local token_type = nil
-        local ok, tokens = pcall(vim.lsp.semantic_tokens.get_at_pos, row, col - 1)
+        local row = vim.api.nvim_win_get_cursor(0)[1] - 1
+        local ok, tokens = pcall(vim.lsp.semantic_tokens.get_at_pos, row, math.max(0, col - 1))
+
         if ok and tokens then
+          local type_likes = {
+            ["type"] = true,
+            ["class"] = true,
+            ["struct"] = true,
+            ["enum"] = true,
+            ["interface"] = true,
+            ["typeParameter"] = true,
+          }
           for _, token in ipairs(tokens) do
-            token_type = token.type
-            break
-          end
-        end
-
-        local type_likes = {
-          ["type"] = true,
-          ["class"] = true,
-          ["struct"] = true,
-          ["enum"] = true,
-          ["interface"] = true,
-          ["typeParameter"] = true,
-        }
-        if token_type and type_likes[token_type] then
-          return true
-        end
-
-        if token_type == nil or token_type == "unresolvedReference" or token_type == "variable" then
-          local last_word = prefix:match("[%w_]+$")
-          if last_word then
-            if prefix:match(":[%s]*[%w_]+$") or last_word:match("^[A-Z]") then
+            if type_likes[token.type] then
               return true
             end
           end
         end
 
-        -- Turbofish (size_of::<...>)
-        if prefix:match("::[:]*$") then
+        if prefix:match("::[:]*$") or prefix:match("[%>%)%]]$") then
           return true
         end
-
-        -- Nest (Vec<Vec<T>>)
-        if prefix:match("[%>%)%]]$") then
+        local last_word = prefix:match("[%w_]+$")
+        if last_word and (prefix:match(":[%s]*[%w_]+$") or last_word:match("^[A-Z]")) then
           return true
         end
 
@@ -74,8 +53,18 @@ return {
           dosuround = true,
           multiline = false,
           bs = true,
-          cond = function(fn)
-            return is_lsp_generic_context()
+          cond = function(fns, o, m)
+            local char_after = o.line:sub(o.col + 1, o.col + 1)
+
+            if char_after == ">" then
+              return true
+            end
+
+            if fns.is_start_pair() then
+              return is_generic(o)
+            end
+
+            return true
           end,
           space = true,
           surround = true,
@@ -87,4 +76,5 @@ return {
     "nvim-mini/mini.pairs",
     enabled = false,
   },
+  { "RRethy/nvim-treesitter-endwise" },
 }
